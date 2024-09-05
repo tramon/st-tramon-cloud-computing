199af88f58dc89111aa38fede32db276f33a71e7
tramon <andriitramon@gmail.com>
2024-09-05 22:30:37 +0300

    redirect fix-02

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
 from flask import Flask
-from flask import request, redirect, url_for
+from flask import request, redirect
 from flask import render_template
 from db.Dao import Dao
-import urllib
 
 app = Flask(__name__)
 
 dao = Dao()
-base_url = "https://st-tramon-cloud-computing-web.onrender.com"
 
 
 @app.route("/", methods=['GET'])
 def home():
     dao.connect_to_db()
     dataset = dao.read_all()
-
     return render_template('index.html', objects=dataset)
 
 
@@ -23,7 +20,7 @@ def add():
     task_to_add = request.form['add']
     dao.connect_to_db()
     dao.insert_without_id(task_to_add)
-    return redirect(url_for(base_url), code=307)
+    return redirect(request.referrer)
 
 
 @app.route("/delete", methods=['POST'])
@@ -31,7 +28,7 @@ def delete():
     id_to_delete = request.form['delete']
     dao.connect_to_db()
     dao.delete_by_id(id_to_delete)
-    return redirect(url_for(base_url), code=307)
+    return redirect(request.referrer)
 
 
 if __name__ == "__main__":
