d403ecb62c04296ba572814abb50d21b3b14c097
tramon <andriitramon@gmail.com>
2024-09-12 16:45:10 +0300

    db close connection fix 8.6

--- db/Dao.py
+++ db/Dao.py
@@ -32,20 +32,10 @@ class Dao:
         Dao.cursor.row_factory = class_row(Tasks)
 
     @staticmethod
-    def read_top_100():
-        select_all_query = "SELECT * FROM tasks "
-        "ORDER BY id "
-        "FETCH FIRST 100 ROWS WITH TIES"
-        with psycopg.connect(
-                dbname=Dao.dbname,
-                user=Dao.user,
-                password=Dao.password,
-                host=Dao.host,
-                port=Dao.port
-        ) as conn:
-            with conn.cursor() as cur:
-                cur.row_factory = class_row(Tasks)
-                return cur.execute(select_all_query).fetchall()
+    def read_top_100(select_all_query="SELECT * FROM tasks "
+                                      "ORDER BY id "
+                                      "FETCH FIRST 100 ROWS WITH TIES"):
+        return Dao.cursor.execute(select_all_query).fetchall()
 
     @staticmethod
     def insert(task_id, task):
@@ -70,17 +60,8 @@ class Dao:
         query_delete_task_by_id = sql.SQL("DELETE FROM {table} WHERE {field} = %s").format(
             table=sql.Identifier(Dao.table_name_tasks),
             field=sql.Identifier(Dao.field_id))
-        with psycopg.connect(
-                dbname=Dao.dbname,
-                user=Dao.user,
-                password=Dao.password,
-                host=Dao.host,
-                port=Dao.port
-        ) as conn:
-            with conn.cursor() as cur:
-                cur.row_factory = class_row(Tasks)
-                cur.execute(query_delete_task_by_id, [task_id])
-                conn.connection.commit()
+        Dao.cursor.execute(query_delete_task_by_id, [task_id])
+        Dao.connection.commit()
 
 
 @dataclass
