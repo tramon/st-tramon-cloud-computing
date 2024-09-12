cd50fca088c8817859cf8c671207adc180f9580d
tramon <andriitramon@gmail.com>
2024-09-12 15:10:49 +0300

    db close connection fix 3

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ dao = Dao()
 
 @app.route("/", methods=['GET'])
 def home():
+    dao.connect_to_db()
     json_dataset = dao.read_top_100()
+    dao.close_connection()
     return render_template('index.html', objects=json_dataset)
 
 
@@ -19,7 +21,9 @@ def home():
 def add():
     task_to_add = request.form['add']
     if task_to_add is not "":
+        dao.connect_to_db()
         dao.insert_without_id(task_to_add)
+        dao.close_connection()
     return redirect(request.referrer)
 
 
@@ -27,13 +31,17 @@ def add():
 def delete():
     id_to_delete = request.form['delete']
     if id_to_delete.isdigit():
+        dao.connect_to_db()
         dao.delete_by_id(id_to_delete)
+        dao.close_connection()
     return redirect(request.referrer)
 
 
 @app.route("/api/get", methods=['GET'])
 def get_all():
+    dao.connect_to_db()
     json_dataset = dao.read_top_100()
+    dao.close_connection()
     return json_dataset
 
 
@@ -41,7 +49,9 @@ def get_all():
 def put():
     task_to_add = request.form['put']
     if task_to_add is not "":
+        dao.connect_to_db()
         dao.insert_without_id(task_to_add)
+        dao.close_connection()
         return return_success(201)
 
 
@@ -49,7 +59,9 @@ def put():
 def delete_via_api():
     id_to_delete = request.form['delete']
     if id_to_delete.isdigit():
+        dao.connect_to_db()
         dao.delete_by_id(id_to_delete)
+        dao.close_connection()
         return return_success()
 
 
