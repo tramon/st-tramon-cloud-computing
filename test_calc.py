5b78ba524179fae1d06897cd6f7168c07c566c7a
tramon <andriitramon@gmail.com>
2024-10-09 15:45:22 +0300

    lab-03.01 with test failures

--- test_calc.py
+++ test_calc.py
@@ -7,6 +7,6 @@ def test_sum():
     assert sum_calculation(2, 3) == 5
 
 
-@pytest.mark.skipif(reason="known defect")
+#@pytest.mark.skipif(reason="known defect")
 def test_tricky_divide():
     assert tricky_divide(12, 6) == 2
